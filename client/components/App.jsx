8797bc17b7319d73cec263b875db9b0ce2393470
tolylya <tolylya220@gmail.com>
2016-06-10 11:03:59 +0300

    Connect front with back

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,23 +1,57 @@
 import React from 'react';
 
+import NotesStore from '../stores/NotesStore.js';
+import NotesActions from '../actions/NotesActions';
+
 import NoteEditor from './NoteEditor.jsx';
 import NotesGrid from './NotesGrid.jsx';
 
-import './App.less'
+import './App.less';
+
+function getStateFromFlux(){
+	return {
+		isLoading: NotesStore.isLoading(),
+		notes: NotesStore.getNotes()
+	}
+};
 
 const App = React.createClass({
+	getInitialState(){
+		return getStateFromFlux();
+	}, 
+
+	componentWillMount(){
+		NotesActions.loadNotes();
+	},
+
+	componentDidMount(){
+		NotesStore.addChangeListener(this._onChange);
+	},
+
+	componentWillUnmount(){
+		NotesStore.removeChangeListener(this._onChange);
+	},
+
 	handleNoteAdd(data){
-		console.log(data);
+		NotesActions.createNote(data);
+	},
+
+	handleNoteDelete(note){
+		NotesActions.deleteNote(note.id);
 	},
 
 	render() {
 		return (
 			<div className="App">
-				<h2>NotesApp</h2>
+				<h2 className="App__header">NotesApp</h2>
 				<NoteEditor onNoteAdd={this.handleNoteAdd} />
-				<NotesGrid /> 
+				<NotesGrid notes={this.state.notes} onNoteDelete={this.handleNoteDelete}/> 
 			</div>
 		);
+	},
+
+	_onChange(){
+		this.setState(getStateFromFlux());
 	}
 });
 
--- client/components/NotesGrid.jsx
+++ client/components/NotesGrid.jsx
@@ -1,8 +1,39 @@
 import React from 'react';
 
+import Masonry from 'react-masonry-component';
+
+import Note from './Note.jsx';
+import './NotesGrid.less';
+
 const NotesGrid = React.createClass({
-	render() {
-		return <h1> NotesGrid </h1>
+		render() {
+
+		const masonryOptions = {
+			itemSelector: '.Note',
+			columWidth: 250,
+			gutter: 10,
+			isFitWidth: true
+		};
+
+		return (
+			<Masonry
+                className='NotesGrid'
+                options={masonryOptions}
+            >
+                {
+                    this.props.notes.map(note =>
+                        <Note
+                            key={note.id}
+                            title={note.title}
+                            onDelete={this.props.onNoteDelete.bind(null, note)}
+                            color={note.color}
+                        >
+                            {note.text}
+                        </Note>
+                    )
+                }
+            </Masonry>
+		);
 	}
 });
 
